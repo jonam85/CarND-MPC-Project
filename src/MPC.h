83259b91d6df29b9102ba3a0225bed059adaa931
Manoj Kumar <manojkumar85@gmail.com>
2018-03-28 00:28:24 +0530

    Updated MPC source files

--- src/MPC.h
+++ src/MPC.h
@@ -6,15 +6,24 @@
 
 using namespace std;
 
+extern size_t N;
+extern size_t x_start;
+extern size_t y_start;
+extern size_t delta_start;
+extern size_t a_start;
+
 class MPC {
  public:
   MPC();
+  
+  std::vector<long> cost_value;
 
   virtual ~MPC();
 
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuatotions.
   vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  void UpdateCost(vector<long> cost_value);
 };
 
 #endif /* MPC_H */
--- src/main.cpp
+++ src/main.cpp
@@ -65,11 +65,29 @@ Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
   return result;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+  
   uWS::Hub h;
+  
+  // Use the command line arguments for cost function
+  std::vector<long> cost_value (7,1);
+  
+  // Use the default values when no command line values
+  cost_value[4] = 50;  
+  cost_value[5] = 250000;
+  cost_value[6] = 1;
+  
+  if((argc > 1) && (argc <= 8))
+  {
+    for(unsigned int i = 0; i < (argc - 1); i++)
+    {
+      cost_value[i] = atol(argv[i+1]);
+    }
+  }
 
   // MPC is initialized here!
   MPC mpc;
+  mpc.UpdateCost(cost_value);
 
   h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -91,35 +109,109 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          
+          double steer_value_i = j[1]["steering_angle"];
+          double throttle_value_i = j[1]["throttle"];
 
-          /*
-          * TODO: Calculate steering angle and throttle using MPC.
-          *
-          * Both are in between [-1, 1].
-          *
-          */
-          double steer_value;
-          double throttle_value;
 
+
+
+          // Add in the latency time for processing
+          double delay_t = .1;
+          
+          // Lf is the distance of the steering axle from COG
+          const double Lf = 2.67;
+          
+          //Display the waypoints/reference line
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+          
+          // loop through all observations
+          for (unsigned int j = 0; j < ptsx.size(); j++)
+          {
+      
+            double veh_x, veh_y;
+            
+            // Convert vehicle co-ordinates to map co-ordinates
+            veh_x = (ptsx[j] - px) * cos(-psi) - (ptsy[j] - py) * sin(-psi);
+            veh_y = (ptsy[j] - py) * cos(-psi) + (ptsx[j] - px) * sin(-psi);
+            
+            next_x_vals.push_back(veh_x);
+            next_y_vals.push_back(veh_y);            
+          }
+          
+          // Convert the vectors to Eigen VectorXd before using in polyfit
+          Eigen::Map<Eigen::VectorXd> next_x_vals_eig(next_x_vals.data(), next_x_vals.size());
+          Eigen::Map<Eigen::VectorXd> next_y_vals_eig(next_y_vals.data(), next_y_vals.size());
+          
+          // Find the coefficients of the best fit polynomial from the given waypoints
+          auto coeffs = polyfit(next_x_vals_eig, next_y_vals_eig, 3);
+          
+          // Calculate the error at x=0, the position just in front of car for CTE
+          double cte = polyeval(coeffs,0);
+          
+          // Calculation for Euclidean distance of last way point //Not used in this code
+          double euc_d = fabs(sqrt((next_x_vals[-1] * next_x_vals[-1]) 
+                  + ( polyeval(coeffs,next_x_vals[-1]) *  polyeval(coeffs,next_x_vals[-1]))));
+                  
+          //std::cout << euc_d << std::endl;
+          
+          // Calculate the error in orientation of the vehicle
+          double epsi = -atan(coeffs[1]);
+          
+          
+          // State after delay.          
+          //transition from t to t+1 (time step is delta_t = 0.1)
+          // using the general kinematic equations
+          
+          double delay_x = v * cos(0) * delay_t; // Assuming initial x = 0 in car co-ordinate
+          double delay_y = v * sin(0) * delay_t; // Assuming initial y = 0 in car co-ordinate
+          double delay_psi = -v * steer_value_i * delay_t / Lf ; // Assuming initial psi = 0 in car co-ordinate
+          double delay_v = v + throttle_value_i * delay_t; 
+          double delay_cte = cte + v * sin(epsi) * delay_t;
+          double delay_epsi = epsi-(v * steer_value_i * delay_t / Lf);
+          
+          // Update the states to be used in ipopt solver
+          Eigen::VectorXd state(6);
+          state << delay_x, delay_y, delay_psi, delay_v, delay_cte, delay_epsi;
+          //state <<  0.,  0., 0., v, cte, epsi;
+          
+          // Solve for the set constraints and get the target variable values
+          std::vector<double> vars = mpc.Solve(state, coeffs);
+
+          // Update the control values to be sent to the simulator
+          double steer_value = vars[0] /(deg2rad(25));
+          double throttle_value = vars[1];
+          
           json msgJson;
+          
+
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
-
+          
           //Display the MPC predicted trajectory 
-          vector<double> mpc_x_vals;
-          vector<double> mpc_y_vals;
+          
+          std::vector<double> x_val;
+          std::vector<double> y_val;
+          
+          for (unsigned int i = 2; i < vars.size(); i++)
+          {
+            // Alternate values for x and y on the trajectory
+            if ( i % 2 == 0 ) {
+              x_val.push_back( vars[i] );
+            } else {
+              y_val.push_back( vars[i] );
+            }
+          }
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
 
-          msgJson["mpc_x"] = mpc_x_vals;
-          msgJson["mpc_y"] = mpc_y_vals;
-
-          //Display the waypoints/reference line
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
+          msgJson["mpc_x"] = x_val;
+          msgJson["mpc_y"] = y_val;
+          
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
@@ -129,7 +221,7 @@ int main() {
 
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          //std::cout << msg << std::endl;
           // Latency
           // The purpose is to mimic real driving conditions where
           // the car does actuate the commands instantly.
